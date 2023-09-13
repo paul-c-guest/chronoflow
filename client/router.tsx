6d430da9c4a50b5bdbde06c6fc2fbc49ca8331a3
Stanley Mair <stanleygmair@gmail.com>
2023-09-13 17:31:23 +1200

    Set up react router

---
+++ client/router.tsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+
+import App from './components/App.js'
+
+export const routes = createRoutesFromElements(
+  <Route path="/" element={<App />}></Route>
+)
+
+export const router = createBrowserRouter(routes)
