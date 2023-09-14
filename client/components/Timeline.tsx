1523956e6c4cea11e80e328cbf23dd083e22a8b9
Paul Guest <paul.christopher.guest@gmail.com>
2023-09-14 15:35:27 +1200

    style and position timeline slider with dedicated css collection

--- client/components/Timeline.tsx
+++ client/components/Timeline.tsx
@@ -1,5 +1,45 @@
+// import { useState } from 'react'
+
 function Timeline() {
-  return <h2>Timeline component</h2>
+  // TESTING / EXPLORATION RE MOUSE EVENTS AND INPUT RANGE SLIDER
+
+  // const [sliderX, setSliderX] = useState(0)
+
+  // const mDown = (event: React.MouseEvent<HTMLInputElement>) => {
+  //   setSliderX(event.clientX)
+  //   console.log('down', event.clientX)
+  // }
+
+  // const mMove = (event: React.MouseEvent<HTMLInputElement>) => {
+  //   if (event.buttons && event.movementX !== 0) {
+  //     console.log(event.movementX > 0 ? 'right' : 'left')
+  //   }
+  // }
+
+  return (
+    <>
+      <h2>Timeline component</h2>
+      <div id="timeline-container">
+        <input
+          id="main-timeline"
+          // onMouseDown={mDown}
+          // onMouseMove={mMove}
+          type="range"
+          min={0}
+          max={100}
+          list="events"
+        />
+
+        {/* TODO: populate this datalist with option elements from an array of events */}
+        <datalist id="events">
+          {/* TODO: remove these hardcoded event positions -- note that values are between slider min/max  */}
+          <option value="26" />
+          <option value="67" />
+          <option value="98" />
+        </datalist>
+      </div>
+    </>
+  )
 }
 
 export default Timeline
