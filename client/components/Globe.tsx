ddbaa4c3ef5ae1d480ec87c91fdfa4ce64bcb36a
Eden Pearson <pearsoneden@gmail.com>
2023-09-14 21:06:38 +1200

    ugly % loading indicator

--- client/components/Globe.tsx
+++ client/components/Globe.tsx
@@ -1,7 +1,19 @@
 /* eslint-disable react/no-unknown-property */
 
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera, useGLTF } from '@react-three/drei'
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  useGLTF,
+  useProgress,
+} from '@react-three/drei'
+import { Suspense } from 'react'
+
+function Loader() {
+  const { active, progress, errors, item, loaded, total } = useProgress()
+  return <Html center>{Math.floor(progress)} % loaded</Html>
+}
 
 function GlobeModel() {
   const { scene } = useGLTF('/assets/globe/scene.gltf')
@@ -17,23 +29,25 @@ function Globe() {
   return (
     <div className="w-1/2 h-[36rem]">
       <Canvas>
-        <PerspectiveCamera
-          makeDefault
-          position={[0, 0, 40]}
-          fov={75}
-          aspect={window.innerWidth / window.innerHeight}
-          near={0.1}
-          far={1000}
-        />
-        <ambientLight intensity={1} />
-        <OrbitControls
-          enableZoom={false}
-          // maxPolarAngle={0.5}
-          // minPolarAngle={0}
-        />
-        <group position={[0, 15, 0]}>
-          <GlobeModel />
-        </group>
+        <Suspense fallback={<Loader />}>
+          <PerspectiveCamera
+            makeDefault
+            position={[0, 0, 40]}
+            fov={75}
+            aspect={window.innerWidth / window.innerHeight}
+            near={0.1}
+            far={1000}
+          />
+          <ambientLight intensity={1} />
+          <OrbitControls
+            enableZoom={false}
+            // maxPolarAngle={0.5}
+            // minPolarAngle={0}
+          />
+          <group position={[0, 15, 0]}>
+            <GlobeModel />
+          </group>
+        </Suspense>
       </Canvas>
     </div>
   )
