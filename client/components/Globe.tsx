2e5a0275a4a8f62178d38d8895fd0d79b8a125e4
Eden Pearson <pearsoneden@gmail.com>
2023-09-19 09:48:13 +1200

    globe with points

--- client/components/Globe.tsx
+++ client/components/Globe.tsx
@@ -4,32 +4,59 @@ import {
   Html,
   OrbitControls,
   PerspectiveCamera,
-  useGLTF,
   useProgress,
 } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Suspense } from 'react'
-
-function Loader() {
-  const { active, progress, errors, item, loaded, total } = useProgress()
-  return <Html center>{Math.floor(progress)} % loaded</Html>
-}
+import { Suspense, useRef, useLayoutEffect, useEffect, useMemo } from 'react'
+import ThreeGlobe from 'three-globe'
 
 function GlobeModel() {
-  const { scene } = useGLTF('/assets/globe/scene.gltf')
+  // const N = 300
+  // const gData = [...Array(N).keys()].map(() => ({
+  //   lat: (Math.random() - 0.5) * 180,
+  //   lng: (Math.random() - 0.5) * 360,
+  //   size: Math.random() / 3,
+  //   color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
+  // }))
+
+  const gData = [
+    {
+      lat: 37.0902,
+      lng: -95.7129,
+      size: Math.random() / 3,
+      color: 'red',
+    },
+    {
+      lat: -40.9006,
+      lng: 174.886,
+      size: Math.random() / 3,
+      color: 'green',
+    },
+  ]
+
+  const globe = new ThreeGlobe({ animateIn: true })
+
+  globe
+    .globeImageUrl('/assets/earth-blue-marble.jpg')
+    .bumpImageUrl('/assets/earth-topology.png')
+    .pointsData(gData)
+    .pointAltitude('size')
+    .pointColor('color')
 
   useFrame(({ clock }) => {
-    scene.rotation.y = clock.getElapsedTime() * 0.5
+    globe.rotation.y = clock.getElapsedTime() * 0.5
   })
 
-  return <primitive object={scene} scale={[2, 2, 2]} />
+  return (
+    <primitive object={globe} position={[0, 0, 0]} scale={[0.2, 0.2, 0.2]} />
+  )
 }
 
 function Globe() {
   return (
     <div className="w-[45%] h-[36rem]">
       <Canvas>
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={null}>
           <PerspectiveCamera
             makeDefault
             position={[0, 0, 40]}
@@ -39,8 +66,8 @@ function Globe() {
             far={1000}
           />
           <ambientLight intensity={1} />
-          <OrbitControls enableZoom={false} />
-          <group position={[0, 20, -5]}>
+          <directionalLight color="white" position={[0, 0, 5]} />
+          <group position={[0, 0, 0]}>
             <GlobeModel />
           </group>
         </Suspense>
@@ -51,5 +78,6 @@ function Globe() {
 
 export default Globe
 
-// maxPolarAngle={0.1}
-// minPolarAngle={0}
+{
+  /* <OrbitControls enableZoom={false} /> */
+}
