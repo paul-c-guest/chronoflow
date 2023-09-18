7acc9fbed1e2ff806843c3524814fa8fb8c88828
Tori Stevens <tori.stevens13@gmail.com>
2023-09-18 12:11:51 +1200

    feat: db function adjusted to camel not snake

--- server/db/functions.ts
+++ server/db/functions.ts
@@ -6,5 +6,13 @@ export async function getAllInventions() {
   return await db('inventions').select('')
 }
 export async function getAllFamousPeople() {
-  return await db('famous_people').select('')
+  return await db('famous_people').select(
+    'id',
+    'name',
+    'year_born as yearBorn',
+    'year_died as yearDied',
+    'country',
+    'known_for as knownFor',
+    'image'
+  )
 }
