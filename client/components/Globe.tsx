8f679f0d1a365a7fe9e0bb530cf00f3908984d6c
Eden Pearson <pearsoneden@gmail.com>
2023-09-19 22:12:41 +1200

    rotates to coords but rotation broken

--- client/components/Globe.tsx
+++ client/components/Globe.tsx
@@ -16,17 +16,20 @@ import {
   useState,
 } from 'react'
 import ThreeGlobe from 'three-globe'
+import * as turf from '@turf/turf'
 import { getCountryCode } from '../helpers'
 
-function GlobeModel({ countryData }) {
-  const [altitude, setAltitude] = useState(0)
-  const globe = new ThreeGlobe({ animateIn: true })
+function GlobeModel({ countryData, centroidCoordinates }) {
+  // inside globemodel add a useEffect to rotate the globe to centroid coords
+  // need to convert center co-ords to a rotation value
+  // apply rotation
+  const globe = useRef(new ThreeGlobe({ animateIn: true }))
 
-  globe
+  globe.current
     .globeImageUrl('/assets/earth-blue-marble.jpg')
     .bumpImageUrl('/assets/earth-topology.png')
   if (countryData) {
-    globe
+    globe.current
       .polygonsData(countryData)
       .polygonCapColor(() => 'rgba(236, 3, 252, 0.6)')
       .polygonSideColor(() => 'rgba(236, 3, 252, 0.1)')
@@ -34,18 +37,37 @@ function GlobeModel({ countryData }) {
       .polygonAltitude(0.05)
   }
 
+  useEffect(() => {
+    console.log(centroidCoordinates)
+    if (centroidCoordinates) {
+      const phi = (90 - centroidCoordinates[0]) * (Math.PI / 180)
+      const theta = (centroidCoordinates[1] + 180) * (Math.PI / 180)
+      globe.current.rotation.x = phi
+      globe.current.rotation.y = theta
+    }
+  }, [centroidCoordinates])
+
   useFrame(({ clock }) => {
-    globe.rotation.y = clock.getElapsedTime() * 0.5
+    if (!centroidCoordinates) {
+      globe.current.rotation.y += clock.getElapsedTime() * 0.5
+    }
   })
 
   return (
-    <primitive object={globe} position={[0, 0, 0]} scale={[0.15, 0.15, 0.15]} />
+    <primitive
+      object={globe.current}
+      position={[0, 0, 0]}
+      scale={[0.15, 0.15, 0.15]}
+    />
   )
 }
 
 function Globe({ selectedCountry }) {
   const [countriesData, setCountriesData] = useState(null)
   const [countryCode, setCountryCode] = useState(null)
+  const [centroidCoordinates, setCentroidCoordinates] = useState<
+    null | number[]
+  >(null)
 
   useEffect(() => {
     const code = getCountryCode(selectedCountry)
@@ -53,16 +75,37 @@ function Globe({ selectedCountry }) {
     setCountryCode(code)
   }, [selectedCountry])
 
+  // TODO:
+  // calculate center of polygon
+  // update state for center coords
+  // use these co-ords as a prop for GlobeModel
+
   useEffect(() => {
     if (countryCode !== null) {
       // Load and parse the GeoJSON file
       fetch('/assets/ne_110m_admin_0_countries.geojson')
         .then((response) => response.json())
         .then((data) => {
-          // Store the country/ies data in state
-          setCountriesData(
-            data.features.filter((d) => d.properties.ISO_A2 == countryCode)
+          // Find the single feature representing the selected country
+          const countryFeature = data.features.find(
+            (d) => d.properties.ISO_A2 === countryCode
           )
+
+          if (countryFeature) {
+            // Find the centroid of the country's polygon
+            const centroid = turf.centroid(countryFeature)
+
+            // Set the centroid coordinates
+            setCentroidCoordinates([
+              centroid.geometry.coordinates[1],
+              centroid.geometry.coordinates[0],
+            ])
+          } else {
+            console.error(`Country with code ${countryCode} not found`)
+          }
+
+          // Set the country data
+          setCountriesData([countryFeature])
         })
         .catch((error) => {
           console.error('Error loading GeoJSON:', error)
@@ -89,7 +132,10 @@ function Globe({ selectedCountry }) {
           <ambientLight intensity={1} />
           <directionalLight color="white" position={[0, 0, 5]} />
           <group position={[0, 0, 0]}>
-            <GlobeModel countryData={countriesData} />
+            <GlobeModel
+              countryData={countriesData}
+              centroidCoordinates={centroidCoordinates}
+            />
           </group>
         </Suspense>
       </Canvas>
